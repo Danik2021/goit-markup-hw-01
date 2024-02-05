ba68c4c01b78f2c500a4a157c7d5f93098aa8650
Danik2021 <danialyakimenko@gmail.com>
2024-02-05 12:34:13 +0100

    add all files

---
+++ script.js
@@ -0,0 +1,6 @@
+let str = 'lorem ipsum dolor first second third';
+let answer = 23 + str;
+console.log(typeof answer);
+
+// If i add number to string answer typeof = string
+// Якщо додати строку до намберу typeof буде string
